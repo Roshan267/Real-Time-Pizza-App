678a507a1b6145f623d6f6b830b652451873667a
Roshan Khorate <roshankhorate26@gmail.com>
2024-06-21 15:59:38 +0530

    add cart page.

--- server.js
+++ server.js
@@ -9,16 +9,37 @@ const PORT = process.env.PORT || 3000
 
 //Assets it is used for to connect the css on the sever 
 app.use(express.static('Public'))
-app.get('/', (req,res)=>{
 
-    res.render('home');
 
-})
+
+
 // set template engine 
 
 app.use(expressLayout);
 app.set('views', path.join(__dirname,'/resources/views'))
 app.set('view engine', 'ejs')
+
+app.get('/', (req,res)=>{
+
+    res.render('home');
+
+})
+
+app.get('/cart', (req,res)=>{
+
+    res.render('customer/cart');
+
+})
+app.get('/login', (req,res)=>{
+
+    res.render('auth/login');
+
+})
+app.get('/register', (req,res)=>{
+
+    res.render('auth/register');
+
+})
 
 
 app.listen(PORT, () =>{
